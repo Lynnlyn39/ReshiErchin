236bb9b65748f91fdbfaabe13a327a8beb024423
jiyarza <jiyarza@gmail.com>
2023-06-17 19:03:28 +0200

    Recipes cleanup

--- Assets/Scripts/Book/RecipeSO.cs
+++ Assets/Scripts/Book/RecipeSO.cs
@@ -9,4 +9,5 @@ public class Recipe : InventoryItemSO
     [SerializeField] private PreparationType _preparationType;
     [SerializeField] private List<RecipeIngredient> _ingredients = new List<RecipeIngredient>();
     public List<RecipeIngredient> Ingredients { get => _ingredients; set => _ingredients = value; }
+    public PreparationType PreparationType { get => _preparationType; set => _preparationType = value; }
 }
