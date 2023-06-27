4c4feb6c384c1fb768de0d3da749f7882bd729fe
Corrrvo <corvo.hisui@gmail.com>
2023-06-27 12:43:00 +0200

    player simple camera follow

---
+++ Assets/Scripts/EnemyAI/EnemyAi.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    [Header("Patrolling")]
+    public Vector3 walkPoint;
+    private bool _walkPointSet;
+    public float walkPointRange;
+
+    [Header("Attacking")]
+    public float timeBetweenAttacks;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+}
